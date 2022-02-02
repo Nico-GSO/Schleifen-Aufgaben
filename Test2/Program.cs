8aab2ad5d3cbac92354427abf2bf5bba0b7e9392
Nico Billig <nico.b37@gso.schule.koeln>
2022-02-02 11:04:12 +0100

    Projektdateien hinzufügen.

---
+++ Test2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int x=0;
+
+            int i = -1;
+
+            Console.WriteLine("PIN eingeben: ");
+            Convert.ToInt32(Console.ReadLine());
+
+            while (x != 1234)
+            {
+                i = i + 1;
+                Console.WriteLine("PIN ist falsch");
+            }
+
+
+
+
+        }
+    }
+}
+
+
+

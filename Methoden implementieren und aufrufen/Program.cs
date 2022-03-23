3348f1198b513ca51273ab66aaac4fef98fa5e13
Nico Billig <nico.b37@gso.schule.koeln>
2022-03-23 10:59:30 +0100

    Methoden angefangen.

---
+++ Methoden implementieren und aufrufen/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Methoden_implementieren_und_aufrufen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int anzahl = 0;
+            anzahl = ausgabeTextMalX(      ,   );
+
+            Console.Write("\nAnzahl der Buchstaben: {0}", anzahl);
+
+        }
+
+        static (int, string) ausgabeTextMalX(string, int)
+
+
+        
+    }
+}
---
+++ Methoden/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Methoden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double zahl = 5;
+
+            double y = ZahlenQuadrat(zahl);
+
+        }
+
+        static double ZahlenQuadrat(double x)
+        {
+
+            double y = x * x;
+            return y;
+        }
+    }
+}

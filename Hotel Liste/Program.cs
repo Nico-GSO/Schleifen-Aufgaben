7f58cded4a94ea06034fda62ab5f957956f7b5a9
Nico Billig <nico.b37@gso.schule.koeln>
2022-03-30 12:54:03 +0200

    Hotel Liste angefangen.

---
+++ Hotel Liste/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+namespace Hotel_Liste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool richtig = false;
+            bool richtig2 = false;
+            string nachname;
+            short zimmernummer;
+            string[] zimmernummern = { "45", "23", "3", "32" };
+            string[] vornamen = { "John", "Paul", "Geroge", "Ringo" };
+            string[] nachnamen = { "Lennon", "McCartney", "Harrison", "Starr" };
+            short[] übernachtungen = { 1, 4, 12, 3 };
+            double[] rechnungsbetrag = { 82.46, 329.84, 989.52, 247.38 };
+
+
+
+            Console.WriteLine("Bitte geben Sie ihren Nachnamen oder ihre Zimmernummer ein:");
+
+
+            nachname = Console.ReadLine();
+            if (Int16.TryParse(nachname, out zimmernummer))
+            {
+                richtig2 = GetData(richtig);
+            }
+
+
+
+            else
+            {
+
+
+
+            }
+
+
+
+
+
+            Console.WriteLine();
+
+
+
+
+
+
+
+
+
+
+        }
+        static bool GetData(string nachname, string[] nachnamen, string[] vornamen, short[] übernachtungen, double[] rechnungsbetrag, bool richtig)
+        {
+            if (eingabe == nachnamen)
+            {
+                richtig = true;
+            }
+
+
+
+            else if (eingabe == zimmernummern)
+            {
+                richtig = true;
+            }
+
+
+
+            else
+            {
+                ausgabe = "Ihre Eingabe ist falsch.";
+                richtig = false;
+            }
+
+
+
+
+
+            return richtig;
+        }
+
+
+
+        static bool GetData(short zimmernummer, short[] zimmernummern, string[] vornamen, short[] übernachtungen, double[] rechnungsbetrag, bool richtig)
+        {
+            if (eingabe == nachnamen)
+            {
+                ausgabe = "hallo";
+                richtig = true;
+            }
+
+
+
+            else if (eingabe == zimmernummern)
+            {
+                ausgabe = "hallo";
+                richtig = true;
+            }
+
+
+
+            else
+            {
+                ausgabe = "Ihre Eingabe ist falsch.";
+                richtig = false;
+            }
+
+
+
+
+
+            return richtig;
+
+
+
+        }
+    }
+}

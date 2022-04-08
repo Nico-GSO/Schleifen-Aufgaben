85e73306e6d852bdb95a01aae466b893ca2eeaa8
Nico Billig <nico.b37@gso.schule.koeln>
2022-04-08 12:40:29 +0200

    Hotel Liste Neu, abgeändert, teilweise mit Error Abfangung.

--- Hotel Liste/Program.cs
+++ Hotel Liste/Program.cs
@@ -4,107 +4,118 @@ namespace Hotel_Liste
 {
     class Program
     {
-        static void Main(string[] args)
+        public class arrays
         {
-            string eingabe;
-            bool richtig = false;
-            bool richtig2 = false;
-            string nachname;
-            short zimmernummer;
-            short[] zimmernummern = { 45, 23, 3, 32 };
-            string[] vornamen = { "John", "Paul", "Geroge", "Ringo" };
-            string[] nachnamen = { "Lennon", "McCartney", "Harrison", "Starr" };
-            short[] übernachtungen = { 1, 4, 12, 3 };
-            double[] rechnungsbetrag = { 82.46, 329.84, 989.52, 247.38 };
 
-
-
-            Console.WriteLine("Bitte geben Sie ihren Nachnamen oder ihre Zimmernummer ein:");
-
-
-            eingabe = Console.ReadLine();
-            if (Int16.TryParse(eingabe, out zimmernummer))
-            {
-                richtig2 = GetData(richtig);
-            }
-
-
-
-            else
-            {
-
-
-            }
-
-
-
-
-
-           
-
-
-
-
-
-
-
-
-
+            public static int[] index = new int[] { 0, 1, 2, 3 };
+            public static int[] zimmernummern = new int[] { 45, 23, 3, 32 };
+            public static string[] vornamen = new string[] { "John", "Paul", "George", "Ringo" };
+            public static string[] namen = new string[] { "Lennon", "MCCartney", "Harrison", "Starr" };
+            public static int[] dauer = new int[] { 1, 4, 12, 3 };
 
         }
-        static bool GetData(string nachname, string[] nachnamen, string[] vornamen, short[] übernachtungen, double[] rechnungsbetrag, bool richtig, string eingabe)
+
+        static void Main()
         {
-            if (eingabe == nachnamen)
+            
+            int auswahl = 0;
+
+            string zimmernummereingabe = null; string nachnameeingabe = null;
+
+            double betrag = 82.46;
+
+            int aussuchenneu;
+
+            int zimmernummereingabeneu = 0;
+
+            bool richtig = true;
+
+            bool richtig2 = true;
+
+            do
             {
+
                 richtig = true;
-            }
-
-
-
-            else if (eingabe == zimmernummern)
-            {
-                richtig = true;
-            }
-
-
-
-            else
-            {
-                Console.WriteLine("Ihre Eingabe ist falsch.");
-                richtig = false;
-            }
-
-
-
-
-
-            return richtig;
-        }
-
-
-
-        static bool GetData(short zimmernummer, short[] zimmernummern, string[] nachnamen, string[] vornamen, short[] übernachtungen, double[] rechnungsbetrag, bool richtig)
-        {
-
-            for (int i = 0; i == zimmernummer; i++)
-            {
-                if (i == zimmernummer)
+                Console.WriteLine("Möchten sie nach Nachnamen oder Zimmernummern suchen? Geben sie bitte '1' ein für Nachnamen oder '2' für Zimmernummern:");
+                string aussuchen = Console.ReadLine();
+                if (!Int32.TryParse(aussuchen, out aussuchenneu))
                 {
-                    
-                    
-                    Console.WriteLine("{0} {1} {2} {3} {4}", zimmernummern[i], vornamen[i], nachnamen[i], übernachtungen[i], rechnungsbetrag[i]);
+                    richtig = false;
+                    Console.WriteLine("\nBitte geben sie eine gültige Eingabe ein. Bitte drücken sie eine beliebige Taste um es erneut zu versuchen:");
+                    Console.ReadKey();
+                    Console.Clear();
                 }
+
+            } while (richtig == false);
+
+            Console.WriteLine();
+
+            switch (aussuchenneu)
+            {
+                case 1:
+                    Console.WriteLine("Geben sie bitte den Nachnamen von derjenigen Person, dessen Profil sie suchen möchten, ein:");
+                    nachnameeingabe = Console.ReadLine();
+                    auswahl = 1;
+                    break;
+
+
+
+                case 2:
+                    do
+                    {
+                        richtig2 = true;
+                        Console.WriteLine("Geben sie bitte die Zimmernummer von derjenigen Person, dessen Profil sie suchen möchten, ein:");
+                        zimmernummereingabe = Console.ReadLine();
+                        if (!Int32.TryParse(zimmernummereingabe, out zimmernummereingabeneu))
+                        {
+                            richtig2 = false;
+                            Console.WriteLine("\nBitte geben sie eine gültige Eingabe ein. Bitte drücken sie eine beliebige Taste um es erneut zu versuchen:");
+                            Console.ReadKey();
+                            Console.Clear();
+                        }
+                        auswahl = 2;
+
+                    } while (richtig2 == false);
+
+                    break;
+
+
+
+                default:
+
+
+
+                    break;
+
             }
 
-           
 
 
+            var data = GetData(zimmernummereingabeneu, nachnameeingabe, auswahl);
 
-
-            return richtig;
-
-
+            Console.WriteLine($"\n\n{"Index",7} |{"Zimmernummer",13} |{"Vorname",9} |{"Nachname",10} |{"Übernachtungen",16} |{"Rechnungsbetrag",17}");
+            Console.WriteLine($"{data.Item2,7} |{data.Item3,13} |{data.Item4,9} |{data.Item5,10} |{data.Item6,16} |{betrag * data.Item6,17}\n");
 
         }
+
+        public static (bool, int, int, string, string, int) GetData(int zimmernummereingabeneu, string nachnameeingabe, int auswahl)
+        {
+            int value = 0;
+
+            if (auswahl == 1)
+            { value = Array.IndexOf(arrays.namen, nachnameeingabe); }
+            else if (auswahl == 2)
+            { value = Array.IndexOf(arrays.zimmernummern, zimmernummereingabeneu); }
+
+            return (true, arrays.index[value], arrays.zimmernummern[value], arrays.vornamen[value], arrays.namen[value], arrays.dauer[value]);
+        }
+
+
+
+
+
+
+
     }
+
 }
